1917c4d971d8fc580fc998927ddaa03c25b36ff9
miaobin <binx.miao@intel.com>
2019-06-24 09:24:49 +0800

    Add input dimensions[1, 2, 3, 2]

--- test/cts/test_supplement/cts_supplement-all.js
+++ test/cts/test_supplement/cts_supplement-all.js
@@ -7992,7 +7992,49 @@ describe('CTS Supplement Test', function() {
     }
   });
 
-  it('check result for Softmax float with 4D input tensor example', async function() {
+  it('check result for Softmax float with 4D input tensor example-1', async function() {
+    let model = await nn.createModel(options);
+    let operandIndex = 0;
+    let input_value = [10.63, 18.75, 12.91, 9.46, 7.31, 12.48, 9.55, 14.28, 19.07, 15.91, 18.47, 20.08];
+    let output_expect = [0.0002974, 0.9997025, 0.9692314, 0.0307689, 0.0056524, 0.9943474, 0.0087492, 0.9912504, 0.9593017, 0.0406991, 0.1665885, 0.8334112];
+
+    let type1 = {type: nn.FLOAT32};
+    let type0 = {type: nn.TENSOR_FLOAT32, dimensions: [1, 2, 3, 2]};
+    let type0_length = product(type0.dimensions);
+
+    let input = operandIndex++;
+    model.addOperand(type0);
+    let beta = operandIndex++;
+    model.addOperand(type1);
+    let output = operandIndex++;
+    model.addOperand(type0);
+
+    model.setOperandValue(beta, new Float32Array([1.0]));
+    model.addOperation(nn.SOFTMAX, [input, beta], [output]);
+
+    model.identifyInputsAndOutputs([input], [output]);
+    await model.finish();
+
+    let compilation = await model.createCompilation();
+    compilation.setPreference(getPreferenceCode(options.prefer));
+    await compilation.finish();
+
+    let execution = await compilation.createExecution();
+
+    let input_input = new Float32Array(input_value);
+    execution.setInput(0, input_input);
+
+    let output_output = new Float32Array(type0_length);
+    execution.setOutput(0, output_output);
+
+    await execution.startCompute();
+
+    for (let i = 0; i < type0_length; ++i) {
+      assert.isTrue(almostEqualCTS(output_output[i], output_expect[i]));
+    }
+  });
+
+  it('check result for Softmax float with 4D input tensor example-2', async function() {
     let model = await nn.createModel(options);
     let operandIndex = 0;
     let input_value = [
