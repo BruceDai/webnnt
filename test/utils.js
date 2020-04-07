85bb487cd3c20f8f1dfe8c5e13918e05717eb6de
BruceDai <feng.dai@intel.com>
2020-04-07 15:48:05 +0800

    [test] Update test for GNA plugin with <url>?ultra-low

--- test/utils.js
+++ test/utils.js
@@ -89,11 +89,11 @@ function setOptions() {
           "prefer": 'low',
           "iterations": "1"
         };
-      } else if (prefer === "ultra-low-power") {
+      } else if (prefer === "ultra-low") {
         prefer = nn.PREFER_ULTRA_LOW_POWER;
         options = {
           "backend": 'WebML',
-          "prefer": 'ultra-low-power',
+          "prefer": 'ultra-low',
           "iterations": "1"
         };
       }
@@ -109,7 +109,7 @@ function getPreferenceCode(preferenceStr) {
     prefer = nn.PREFER_FAST_SINGLE_ANSWER;
   } else if (preferenceStr === 'low') {
     prefer = nn.PREFER_LOW_POWER;
-  } else if (preferenceStr === 'ultra-low-power') {
+  } else if (preferenceStr === 'ultra-low') {
     prefer = nn.PREFER_ULTRA_LOW_POWER;
   }else {
     console.error('Invalid preference string.');
